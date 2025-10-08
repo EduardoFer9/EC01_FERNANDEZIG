9e8715dbbcd2f43f1b5396e1d19125c61ef63edf
EduardoFer9 <oscacito009@gmail.com>
2025-10-07 21:51:00 -0500

    ACTUALIZADO_EVIDENCIAS

--- EC1Rest-FernandezIgnacio/src/main/java/pe/edu/idat/EC1Rest_FernandezIgnacio/Ec1RestFernandezIgnacioApplication.java
+++ EC1Rest-FernandezIgnacio/src/main/java/pe/edu/idat/EC1Rest_FernandezIgnacio/Ec1RestFernandezIgnacioApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Ec1RestFernandezIgnacioApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Ec1RestFernandezIgnacioApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Ec1RestFernandezIgnacioApplication.class, args);
+    }
 }
--- EC1Rest-FernandezIgnacio/src/main/java/pe/edu/idat/EC1Rest_FernandezIgnacio/controller/FundamentosController.java
+++ EC1Rest-FernandezIgnacio/src/main/java/pe/edu/idat/EC1Rest_FernandezIgnacio/controller/FundamentosController.java
@@ -1,21 +1,23 @@
 package pe.edu.idat.EC1Rest_FernandezIgnacio.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import pe.edu.idat.EC1Rest_FernandezIgnacio.dto.MesDto;
 import pe.edu.idat.EC1Rest_FernandezIgnacio.service.FundamentosService;
 
+@RestController
+@RequestMapping("/api/fundamentos")
 public class FundamentosController {
+
     @GetMapping({"/mes", "/mes/{numero}"})
-    public ResponseEntity<MesDto> obtenerMes(
-            @PathVariable(required = false) Integer numero) {
+    public ResponseEntity<MesDto> obtenerMes(@PathVariable(required = false) Integer numero) {
         if (numero == null) numero = 1; // valor por defecto
+
         MesDto mesDto = new MesDto();
         mesDto.setNumero(numero);
         mesDto.setMes(FundamentosService.obtenerNombreMes(numero));
+
         return ResponseEntity.ok(mesDto);
     }
-
-
 }
+
